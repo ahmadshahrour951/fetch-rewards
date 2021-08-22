36314d84b28b66cb02f600c985b4ae8fd4633d9e
shahrour <ahmadshahrour951@gmail.com>
2021-08-22 10:09:57 -0700

    Add get routes for specific payers

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/add', (req, res, next) => {});
-router.patch('/spend', (req, res, next) => {});
+router.get('/transactions', (req, res, next) => {});
+router.get('/transactions/:payerId', (req, res, next) => {});
 router.get('/balance', (req, res, next) => {});
+router.get('/balance/:payerId', (req, res, next) => {});
+
+router.post('/add', (req, res, next) => {});
+
+router.patch('/spend', (req, res, next) => {});
+
 router.use((error, req, res, next) => {});
 router.use((req, res, next) => {});
 
