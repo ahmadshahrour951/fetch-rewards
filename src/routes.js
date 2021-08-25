9abfb7966ce43b690322abce694f1f843ab61483
shahrour <ahmadshahrour951@gmail.com>
2021-08-24 20:04:37 -0700

    Spend Route

--- src/routes.js
+++ src/routes.js
@@ -47,6 +47,56 @@ router.get('/balance', async (req, res, next) => {
   }
 });
 
+router.patch('/spend', async (req, res, next) => {
+  try {
+    const now = new Date();
+    const points = req.body.points;
+    const transactions = await db.Transaction.findAll({
+      order: [['timestamp', 'ASC']],
+    });
+
+    let pointsLeft = points;
+    let payerDeductions = {};
+
+    for (let transaction of transactions) {
+      if (pointsLeft <= 0) break;
+
+      const subtractNum =
+        pointsLeft - transaction.points < 0 ? pointsLeft : transaction.points;
+
+      if (payerDeductions.hasOwnProperty(transaction.payer)) {
+        payerDeductions[transaction.payer] -= subtractNum;
+      } else {
+        payerDeductions[transaction.payer] = -subtractNum;
+      }
+
+      pointsLeft -= transaction.points;
+    }
+
+    if (pointsLeft > 0) {
+      return res.status(500).json({ message: 'Not enough points to claim.' });
+    }
+
+    let newTransactions = [];
+
+    for (let payer in payerDeductions) {
+      newTransactions.push({
+        payer,
+        points: payerDeductions[payer],
+      });
+
+      await db.Transaction.create({
+        payer,
+        points: payerDeductions[payer],
+        timestamp: now,
+      });
+    }
+
+    return res.status(200).json(newTransactions);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 router.use((error, req, res, next) => {});
 router.use((req, res, next) => {});
